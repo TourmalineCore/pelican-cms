741cb3dfc39cf1a7f209429309293d79b44b4517
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-09-01 16:39:02 +0500

    test: #471: uncomment isPinned field

--- playwright-tests/api-tests/news.spec.ts
+++ playwright-tests/api-tests/news.spec.ts
@@ -47,7 +47,7 @@ async function checkNewsResponseTest({
       slug: '2025/02/15/api-smoke-v-zooparke-poyavilsya-amurskij-tigr',
       date: DATE,
       seo: MOCK_SEO,
-      // isPinned: true
+      isPinned: true
     }
   ];
 
@@ -79,7 +79,7 @@ async function createNews({
           innerContent: INNER_CONTENT,
           date: DATE,
           seo: MOCK_SEO,
-          // isPinned: true
+          isPinned: true
         }
       }
     });
